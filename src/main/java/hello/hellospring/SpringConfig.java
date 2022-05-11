ac51de6d25bb65b9114f1ca6676cb3de159f16e3
upgleman <upgleman@nate.com>
2022-05-11 22:49:01 +0900

    Spring DATA JPA - 인터페이스만으로 JPA 사용

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,40 +1,64 @@
 package hello.hellospring;
 
 import hello.hellospring.aop.TimeTraceAop;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
-    private final EntityManager em;
+    /*
+    DB 연동
+    */
+//    private final DataSource dataSource;
+    /*
+    JPA 연동
+     */
+//    private final EntityManager em;
+    /*
+    생성자를 통한 DI
+     */
+//    public SpringConfig(DataSource dataSource, EntityManager em) {
+//        this.dataSource = dataSource;
+//        this.em = em;
+//    }
 
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+    /*
+    Spring JPA
+     */
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+//    }
+    /*
+    Spring JPA
+     */
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+    @Bean
+    public void memberRepository() {
+//        return new MemoryMemberRepository();
+//        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+
     }
 
     @Bean
     public TimeTraceAop timeTraceAop() {
         return new TimeTraceAop();
-    }
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
     }
 
 }
